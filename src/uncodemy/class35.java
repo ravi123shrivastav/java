53bee384bcf00cf180cbb68db057aba382fe01b9
ravi123shrivastav <ravishrivastav610@gmail.com>
2024-06-05 21:46:05 +0530

    Update class35.java

--- src/uncodemy/class35.java
+++ src/uncodemy/class35.java
@@ -3,8 +3,20 @@ package uncodemy;
 public class class35 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for(int i=2; i <= 100; i++) {
+			
+			System.out.print("Factors of " + i + ": ");
+			for(int j = 2; j < i; j++)
+			if((i%j) == 0)
+			System.out.print(j + " ");
+			System.out.println();
+			}
 
 	}
 
 }
+
+
+	
+
+
